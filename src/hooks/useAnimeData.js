729e39895314b359ff13aca05599993195ec0279
Hiura <hiura0012@gmail.com>
2026-01-23 18:53:55 +0700

    Create useAnimeData.js

---
+++ src/hooks/useAnimeData.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export const useAnimeData = (fetchFunction, ...params) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const result = await fetchFunction(...params);
+        setData(result.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [fetchFunction, ...params]);
+
+  return { data, loading, error };
+};
